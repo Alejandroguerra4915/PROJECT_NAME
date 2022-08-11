680cac2d8821a3d8e62da7bdd1e010cbb8804a1e
Luis Carmona <luisfelipe.carmona.lopez@gmail.com>
2022-08-11 17:30:08 -0500

    test done

--- task1.cpp
+++ task1.cpp
@@ -2,7 +2,42 @@
 #include "task1.h"
 
 
-void task1(){
+void task1()
+{
+    enum class Task1States
+    {
+        INIT,
+        WAIT_DATA
+    };
+    static Task1States task1State = Task1States::INIT;
+
+    switch (task1State)
+    {
+    case Task1States::INIT:
+    {
+        Serial.begin(115200);
+        task1State = Task1States::WAIT_DATA;
+        break;
+    }
+
+    case Task1States::WAIT_DATA:
+    {
+        // evento 1:
+        // Ha llegado al menos un dato por el puerto serial?
+        if (Serial.available() > 0)
+        {
+            Serial.read();
+            Serial.print("Hola computador\n");
+        }
+        break;
+    }
+
+    default:
+    {
+        break;
+    }
+    }
+}
 
     
 }
