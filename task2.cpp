03846eb47d8a3a77f22f31cf93c680dac7bc35b2
M4U-l <83553156+M4U-l@users.noreply.github.com>
2022-08-25 14:39:00 -0500

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,8 +1,71 @@
 #include <Arduino.h>
 #include "task2.h"
+#include "events.h"
 
+void task2()
+{
+    enum class TaskState
+    {
+        INIT,
+        WAIT_PRESS,
+        WAIT_STABLE,
+        WAIT_RELEASE
+    };
+    static TaskState taskState = TaskState::INIT;
+    static uint8_t lastButtonPressed;
+    static uint32_t initStableTime;
 
-void task2(){
+    const uint8_t boton1 = 32;
+    const uint8_t boton2 = 13;
+    const uint32_t tiempo = 100; WAIT_STABLE
 
-    
+    switch (taskState)
+    {
+    case TaskState::INIT:
+    {
+        pinMode(boton1, INPUT_PULLUP);
+        pinMode(boton2, INPUT_PULLUP);
+        taskState = TaskState::WAIT_PRESS; 
+        break;
+    }
+    case TaskState::tiempo:
+    {
+        if(digitalRead(boton1) == LOW){
+            lastButtonPressed = boton1;
+            initStableTime = millis();
+            taskState = TaskState::tiempo;
+        }
+        if(digitalRead(boton2) == LOW){
+            lastButtonPressed = boton2;
+            initStableTime = millis();
+            taskState = TaskState::tiempo;
+        }
+
+        break;
+    }
+    case TaskState::tiempo:
+    {
+        if(digitalRead(lastButtonPressed) != LOW){
+            taskState = TaskState::WAIT_PRESS;
+        }
+        else if ( (millis() - initStableTime) > tiempo){
+            if(lastButtonPressed == boton1) buttonEvt.whichButton = BUTTONS::Boton1;
+            else if(lastButtonPressed == boton2) buttonEvt.whichButton = BUTTONS::Boton2;
+            buttonEvt.trigger = true;
+            //printf("Button pressed: %d\n", lastButtonPressed);
+            taskState = TaskState::WAIT_RELEASE;
+        }
+        break;
+    }
+    case TaskState::WAIT_RELEASE:{
+        if(digitalRead(lastButtonPressed) == HIGH){
+            taskState = TaskState::WAIT_PRESS;
+        }
+        break;
+    }
+
+    default:
+    {
+    }
+    }
 }
